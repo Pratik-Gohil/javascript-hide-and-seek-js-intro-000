f107c535d51ace59a8558acec229fdeb329bc89d
Pratik <pr.tikgohil@gmail.com>
2020-02-25 10:45:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-
+function getFirstSelector(selector) {
+  console.log(document.querySelector(selector));
+  return document.querySelector(selector)
+}
+function nestedTarget() {
+  console.log(document.querySelector("#nested").querySelector(".target"));
+  return document.querySelector("#nested").querySelector(".target")
+}
+function deepestChild() {
+  console.log(document.querySelector("#grand-node").lastChild)
+  return document.querySelector("#grand-node").lastChild
+}
